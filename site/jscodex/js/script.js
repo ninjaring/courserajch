62730d1777b7aa047974c3ce300e4265c88d2e0c
ninjaring <getyourways@gmail.com>
2016-08-23 17:46:54 +0700

    add #3 Math Agent

--- site/jscodex/js/script.js
+++ site/jscodex/js/script.js
@@ -53,4 +53,54 @@ orderside();
 // output area
 orderwhat();
 
-console.log("======= #2 ?");
+// // #2 230816 Math Agent
+console.log("======= #2 230816");
+// // two undefined variables with predefined operation
+// example one
+function alpha(a) {
+  function satu(s1) {
+    return s1 - a;
+  };
+  return satu;
+}
+var minus10 = alpha(10);
+console.log(minus10(2));
+
+// example two
+function harga(rp) {
+  function jumlah(item) {
+    return item * rp;
+  };
+  return jumlah;
+}
+
+var rejoice12s = harga(4900); // define operation
+rejoicebotol = harga(17000);
+sabunall = harga(5000);
+rejoice12s.namabarang = "Rejoice sachet 12s: ";
+console.log(rejoice12s(5)); // then get direct result one by one
+console.log(rejoicebotol(1));
+console.log(sabunall(10));
+
+function transaksi(n, barang) { // or passing functions as arguments
+  return barang(n);
+}
+var hasil = transaksi(12, rejoice12s); // then create 'hasil' var
+console.log(rejoice12s.namabarang + hasil);
+hasil = transaksi(12, rejoicebotol); // -- another 'hasil' var
+console.log(hasil);
+//
+// // #3
+console.log("======= #3 Declaration");
+// // declaration
+function beta(b) {
+  return {
+    dua: function() {
+      console.log(b + " is a number");
+    }
+  }
+}
+
+var info = beta(7);
+info.dua();
+beta(2);
