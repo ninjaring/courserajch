5217c6bd3d68a6ff0ee85c8a70070120ca837086
ninjaring <getyourways@gmail.com>
2016-08-24 23:04:18 +0700

    harder starter resolved

--- site/mod4sol/script.js
+++ site/mod4sol/script.js
@@ -29,19 +29,32 @@ WARNING!!! WARNING!!!
 
 */
 
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    }
+    else {
+      helloSpeaker.speak(names[i]);
+    }
+  };
+})();
+
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+// for (/* fill in parts of the 'for' loop to loop over names array */) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +70,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
-  }
-}
+//   if (/* fill in condition here */) {
+//     // byeSpeaker.xxxx
+//   } else {
+//     // helloSpeaker.xxxx
+//   }
+// }
